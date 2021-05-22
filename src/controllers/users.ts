2956fe7282f0a05cfaa0d26e95be83e8f8905db5
Mahmud Apak <apakmahmud@gmail.com>
2021-05-22 14:45:57 +0300

    Remove authentication check from users endpoint

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -1,9 +1,9 @@
-import { Request, Response } from "express";
-import { genericCatchHandler } from "../utils/generic-catch-handler";
-import { User } from "../database/models/User";
+import { Request, Response } from 'express';
+import { genericCatchHandler } from '../utils/generic-catch-handler';
+import { User } from '../database/models/User';
 
 const getUsers = (req: Request, res: Response): void => {
-  User.findAll({ attributes: ["userId"] })
+  User.findAll({ attributes: ['userId'] })
     .then((users: User[]) => {
       res.send({ users });
     })
--- src/routers/users.ts
+++ src/routers/users.ts
@@ -1,14 +1,11 @@
-import express from "express";
-import usersController from "../controllers/users";
-import verifyToken from "../middlewares/verify-token";
+import express from 'express';
+import usersController from '../controllers/users';
 
 const router = express.Router();
 
 router.use(express.urlencoded());
 router.use(express.json());
 
-router.use(verifyToken);
-
-router.get("/", usersController.getUsers);
+router.get('/', usersController.getUsers);
 
 export default router;
